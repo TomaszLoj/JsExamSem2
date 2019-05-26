a0e274247cc35c95680f097545a91fa57fb60ae9
Tomasz Łój <tomasz.loj@gmail.com>
2019-05-26 17:03:44 +0200

    commit before split to components

---
+++ t1.js
@@ -0,0 +1,14 @@
+/*jshint esversion: 6 */
+function reverseVector(vector) {
+    return [!vector[0], !vector[1]];
+}
+
+let direction = {
+    straight: [1, 1],
+    left: [-1, 1],
+    right: [1, -1]
+};
+
+let wallsAtDirection = direction.
+
+console.log(direction);
